a0b5d01ed89ef8fae0be6bf665331fa27d2f1781
Rod Johnson <johnsonroda@gmail.com>
2018-03-01 11:49:40 -0600

    Initial commit from Atomist

---
+++ src/main/java/atomist/Stl2Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stl2Application.class, args);
+	}
+}
